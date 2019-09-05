f47090087d17dba32dd30e43b613e26a448c70b0
Gabriel Bucalon <gabriel.bucalon@synvia.com.br>
2019-09-05 11:43:35 -0300

    <feat> integração com API do google maps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {} from 'googlemaps';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { }
+  @ViewChild('map') mapElement: any;
+  map: google.maps.Map;
 
+  ngOnInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
   }
 
 }
